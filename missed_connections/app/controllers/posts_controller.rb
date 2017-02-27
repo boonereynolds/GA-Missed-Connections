9cbb1e96191c5643015efc42d7eb92521c2fdb0c
Boone Reynolds <boone.reynolds@gmail.com>
2017-02-27 12:34:01 -0800

    created create action

--- missed_connections/app/controllers/posts_controller.rb
+++ missed_connections/app/controllers/posts_controller.rb
@@ -8,4 +8,18 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(params.require(:post).permit(:title, :body, :location))
+
+    if @post.save
+      redirect_to posts_path
+    else
+      render :new
+    end
+  end
+
 end
--- missed_connections/config/routes.rb
+++ missed_connections/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
   get 'posts/' => 'posts#index'
+  get 'posts/new' => 'posts#new', as: :new_post
   get 'posts/:id' => 'posts#show', as: :post
+  post 'posts/' => 'posts#create'
 
 end
