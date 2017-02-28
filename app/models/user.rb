ef342dd0761816e81a1219430b39bc6b31f42434
Boone Reynolds <boone.reynolds@gmail.com>
2017-02-27 16:27:55 -0800

    implement authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ApplicationRecord
+  # ----- add these lines here: -----
+      has_secure_password
+      # Verify that email field is not blank and that it doesn't already exist in the db (prevents duplicates):
+      validates :email, presence: true, uniqueness: true
+      # ----- end of added lines -----
+
   has_many :posts
   has_many :comments
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,26 @@ Rails.application.routes.draw do
   patch 'posts/:id' => 'posts#update'
   delete 'posts/:id' => 'posts#destroy'
 
+  # ----- add these lines here: -----
+      # Add a root route if you don't have one...
+      # We can use users#new for now, or replace this with the controller and action you want to be the site root:
+      root to: 'users#new'
+      # sign up page with form:
+      get 'users/new' => 'users#new', as: :new_user
+      # create (post) action for when sign up form is submitted:
+      post 'users' => 'users#create'
+
+
+      root to: 'users#new'
+    # get 'users/new' => 'users#new', as: :new_user
+    post 'users' => 'users#create'
+    # ----- add these lines here: -----
+    # log in page with form:
+    get '/login'     => 'sessions#new'
+    # create (post) action for when log in form is submitted:
+    post '/login'    => 'sessions#create'
+    # delete action to log out:
+    delete '/logout' => 'sessions#destroy'
+
+      # ----- end of added lines -----
 end
