6b9979920ec38d9a44025e77fd08e3fcb3c8eb5f
Boone Reynolds <boone.reynolds@gmail.com>
2017-03-02 15:56:15 -0800

    added more styling, updated brand logo

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+if !Location.find(1)
+  Location.create([
+    {campus: Atlanta},
+    {campus: Austin},
+    {campus: Boston},
+    {campus: Chicago},
+    {campus: Denver},
+    {campus: DTLA},
+    {campus: New York City},
+    {campus: Orange County},
+    {campus: Providence},
+    {campus: San Francisco},
+    {campus: Santa Monica},
+    {campus: Seattle},
+    {campus: Toronto},
+    {campus: Washington DC},
+    {campus: London},
+    {campus: Hong Kong},
+    {campus: Singapore},
+    {campus: Brisbane},
+    {campus: Melbourne},
+    {campus: Sydney},
+    ])
+end
